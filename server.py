495f2c40c87a3426104d393aeebf64854df4dc1c
Dan Le <Danhuyle508@gmail.com>
2019-02-26 16:15:50 -0800

    404 error added

--- server.py
+++ server.py
@@ -8,40 +8,61 @@ class OkRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         html = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>cowpy</title>
-</head>
-<body>
-    <nav>
-        <a href="/cow">cowsay</a>
-    </nav>
-    <main style="white-space: pre-wrap;">
-    {}
-    </main>
-</body>
-</html>
-        """
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <title>cowpy</title>
+                </head>
+                <body>
+                    <nav>
+                        <a href="/cow">cowsay</a>
+                    </nav>
+                    <main style="white-space: pre-wrap;">
+                    {}
+                    </main>
+                </body>
+                </html>
+                """
         parsed_path = urlparse(self.path)
         parsed_qs = parse_qs(parsed_path.query)
 
-        if parsed_path.path == '/cows':
-            bun = cow.Bunny()
-            msg = bun.milk(parsed_qs['msg'][0])
+        if parsed_path.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(html.format(' ').encode())
 
+        elif parsed_path.path == '/cows':
+            bun = cow.Bunny()
+            if parsed_qs['msg']:
+                msg = bun.milk(parsed_qs['msg'][0])
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(html.format(msg).encode())
+
+        else:
+            msg = '404 Not Found.'
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(html.format(msg).encode())
 
-    def do_POST(self):
+    # def do_POST(self):
+
         cont_len = int(self.headers.get('Content-Length'))
         post_data = self.rfile.read(cont_len)
         parsed_path = urlparse(self.path)
+        parsed_qs = parse_qs(parsed_path.query)
 
-        if parsed_path == '/cows':
-            pass
+        # if parsed_path.path == '/cows':
+        #     bun = cow.Bunny()
+        #     msg = bun.milk(parsed_qs['msg'][0])
+        #     self.send_response_only(200)
+        #     self.end_headers()
+        #     self.wfile.write(b'<html><body><h1>a POST</html></body></h1>)
+
 
 
 
