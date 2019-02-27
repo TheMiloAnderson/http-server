fc2a3bd1ecede8d48ef3642d3cc49b36f9b6ba34
Jasmin <jasmin.arensdorf@gmail.com>
2019-02-26 17:18:55 -0800

    Coded the cowpy response to be returned as JSON body.

--- server.py
+++ server.py
@@ -49,22 +49,29 @@ class OkRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(html.format(msg).encode())
 
-    # def do_POST(self):
+    def do_POST(self):
 
         cont_len = int(self.headers.get('Content-Length'))
-        post_data = self.rfile.read(cont_len)
+        post_data = self.rfile.read(cont_len).decode()
+        tmp_array = post_data.split("=")
+        tmp_list = tmp_array[1].split("+")
+        
+        print(tmp_list)
+        post_data = ""
+        for i in range(len(tmp_list)):
+            post_data += tmp_list[i] + " "
+        
         parsed_path = urlparse(self.path)
-        parsed_qs = parse_qs(parsed_path.query)
 
-        # if parsed_path.path == '/cows':
-        #     bun = cow.Bunny()
-        #     msg = bun.milk(parsed_qs['msg'][0])
-        #     self.send_response_only(200)
-        #     self.end_headers()
-        #     self.wfile.write(b'<html><body><h1>a POST</html></body></h1>)
-
-
-
+        if parsed_path.path == '/cows':
+            bun = cow.Bunny()
+            msg = bun.milk(post_data)
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            
+            reply = json.dumps(msg)
+            self.wfile.write(reply.encode())
 
 if __name__ == '__main__':
     server_addr = ('', 5000)
